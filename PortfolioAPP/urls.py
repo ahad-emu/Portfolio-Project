31dceea8aa3fd4b8ddb1300d422d2df913186872
ahad-emu <ahademu99@gmail.com>
2020-06-16 20:33:51 +0600

    navbar completed

--- PortfolioAPP/urls.py
+++ PortfolioAPP/urls.py
@@ -3,4 +3,6 @@ from PortfolioAPP import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("portfolio/", views.portfolio, name="portfolio"),
+    path("blog/", views.blog, name="blog"),
 ]
